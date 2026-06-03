86336f4ed2a456f221f68fce97a4be93c940b5d1
Мармулев Семён <marmulev2003@mail.ru>
2026-06-03 21:26:56 +0300

    добавлен pprof

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,8 @@ import (
 	"l45/internal/generator"
 	"l45/internal/httpapi"
 	"l45/internal/repository"
+
+	_ "net/http/pprof"
 )
 
 func main() {
@@ -18,10 +20,20 @@ func main() {
 	mux := http.NewServeMux()
 	handler.RegisterRoutes(mux)
 
+	go func() {
+		pprofAddr := "localhost:6060"
+
+		log.Printf("pprof server started on http://%s/debug/pprof/", pprofAddr)
+
+		if err := http.ListenAndServe(pprofAddr, nil); err != nil {
+			log.Printf("pprof server error: %v", err)
+		}
+	}()
+
 	addr := ":8080"
 
 	log.Printf("api server started on %s", addr)
-	log.Printf("try: http://localhost%s/order?id=order-1", addr)
+	log.Printf("try: curl http://localhost%s/order?id=order-1", addr)
 
 	if err := http.ListenAndServe(addr, mux); err != nil {
 		log.Fatal(err)
